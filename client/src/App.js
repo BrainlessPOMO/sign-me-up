86f5bf8ad68a05185c37b1531da73361670cf715
BrainlessPOMO <agg2elos@gmail.com>
2022-04-11 00:29:05 +0300

    Started the GUI for the login signup page.
    Apla ekana to header tou form.
    Ta login kai signup buttons kai thn allagh sthn js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
 
 function App() {
+  function changeForm(){
+    const loginBtn = document.querySelector("#login")
+    const signupBtn = document.querySelector("#signup")
+
+    loginBtn.addEventListener("click", () => {
+      if(loginBtn.className !== "tab active"){
+        signupBtn.parentElement.classList.remove("active")
+        loginBtn.parentElement.classList.add("active")
+      }
+    })
+    signupBtn.addEventListener("click", () => {
+      if(loginBtn.className !== "tab active"){
+        loginBtn.parentElement.classList.remove("active")
+        signupBtn.parentElement.classList.add("active")
+      }
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="fullBackground" style={{ 
+      backgroundImage: `url(${process.env.PUBLIC_URL + 'images/map_background.png'})`, height: '100vh'
+    }}>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12 col-md-6 col-lg-4 col-xl-3 left-homescreen">
+            <div className="login-form"> 
+              <div className="login-header">
+                <li className="tab active"><a href="#" onClick= {changeForm} id="login">Log In</a></li>
+                <li className="tab"><a href="#" onClick= {changeForm} id="signup">Sign Up</a></li>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
